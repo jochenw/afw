8954a4f325144816e238815487f55027aabf7042
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2019-08-06 06:57:05 +0200

    Simplified the Log4j2LogFactory.

--- afw-core/src/main/java/com/github/jochenw/afw/core/log/log4j/Log4j2LogFactory.java
+++ afw-core/src/main/java/com/github/jochenw/afw/core/log/log4j/Log4j2LogFactory.java
@@ -16,12 +16,12 @@
 package com.github.jochenw.afw.core.log.log4j;
 
 import java.io.InputStream;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
 import java.net.URL;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import com.github.jochenw.afw.core.log.AbstractLog;
 import com.github.jochenw.afw.core.log.AbstractLogFactory;
@@ -37,46 +37,14 @@ public class Log4j2LogFactory extends AbstractLogFactory {
 
 	@Override
 	protected void init() {
-		Class<?> configurationSourceClass = null;
-		Class<?> configuratorClass = null;
-		try {
-			configurationSourceClass = Class.forName("org.apache.logging.log4j.core.config.ConfigurationSource");
-			configuratorClass = Class.forName("org.apache.logging.log4j.core.config.Configurator");
-		} catch (ClassNotFoundException e) {
-			configurationSourceClass = null;
-			configuratorClass = null;
+		final URL url = getResourceLocator().getResource("log4j2.xml");
+		try (InputStream in = url.openStream()) {
+			final ConfigurationSource csource = new ConfigurationSource(in, url);
+			Configurator.initialize(Thread.currentThread().getContextClassLoader(), csource);
+		} catch (Throwable t) {
+			throw Exceptions.show(t);
 		}
-
-		if (configurationSourceClass == null  ||  configuratorClass == null) {
-			// log4j-core is not in the class path
-			logger = LogManager.getLogger(Log4j2LogFactory.class);
-			logger.warn("Log4j-Core not found, configuration is hopefully done elsewhere.");
-		} else {
-			final URL url = getResourceLocator().getResource("log4j2.xml");
-
-			final Constructor<?> cons;
-			try {
-				cons = configurationSourceClass.getConstructor(InputStream.class, URL.class);
-			} catch (Throwable t) {
-				throw Exceptions.show(t);
-			}
-
-			final Method initializeMethod;
-			try {
-				initializeMethod = configuratorClass.getMethod("initialize", ClassLoader.class, configurationSourceClass);
-			} catch (Throwable t) {
-				throw Exceptions.show(t);
-			}
-
-			final Object configurationSource;
-			try (InputStream istream = url.openStream()) {
-				configurationSource = cons.newInstance(istream, url);
-				initializeMethod.invoke(null, null, configurationSource);
-			} catch (Throwable t) {
-				throw Exceptions.show(t);
-			}
-			logger = LogManager.getLogger(Log4j2LogFactory.class);
-			logger.info("Log4j2 configured from " + url);
-		}
+		logger = LogManager.getLogger(Log4j2LogFactory.class);
+		logger.info("Log4j2 configured from " + url);
 	}
 }
