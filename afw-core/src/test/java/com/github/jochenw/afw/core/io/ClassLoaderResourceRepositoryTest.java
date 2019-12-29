460565af3094caa57753052043ff27406b8f70ec
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2019-12-29 11:47:38 +0100

    Another attempt to fix the ClassLoaderResourceRepositoryTest.

--- afw-core/src/test/java/com/github/jochenw/afw/core/io/ClassLoaderResourceRepositoryTest.java
+++ afw-core/src/test/java/com/github/jochenw/afw/core/io/ClassLoaderResourceRepositoryTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.net.URL;
 import java.net.URLDecoder;
+import java.nio.charset.Charset;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,7 +25,6 @@ public class ClassLoaderResourceRepositoryTest {
 	public void testList() {
 		final Set<String> uris = new HashSet<>();
 		final ClassLoaderResourceRepository clrr = new ClassLoaderResourceRepository(getClass().getClassLoader());
-		clrr.setLogger((s) -> System.out.println(s));
 		clrr.list((r) -> uris.add(r.getUri()));
 		assertTrue(uris.contains("javax/servlet/Filter.class"));  // An entry from Maven's "compile" scope.
 		assertTrue(uris.contains("org/junit/Test.class"));       // An entry from Maven's "test" scope.
@@ -42,12 +42,11 @@ public class ClassLoaderResourceRepositoryTest {
 		final String uri = "file:/C:/Program%20Files%20(x86)/Jenkins/workspace/afw-core/afw-core/target/test-classes/META-INF/MANIFEST.MF";
 		final URL url = new URL(uri);
 		assertEquals("file", url.getProtocol());
-		assertEquals("C:/Program Files (x86)/Jenkins/workspace/afw-core/afw-core/target/test-classes/META-INF/MANIFEST.MF", URLDecoder.decode(url.getFile()));
+		assertEquals("/C:/Program Files (x86)/Jenkins/workspace/afw-core/afw-core/target/test-classes/META-INF/MANIFEST.MF", URLDecoder.decode(url.getFile(), Charset.defaultCharset().name()));
 	}
 
 	private void assertResourceFound(final Set<String> pUris, final ClassLoaderResourceRepository pRepository, String pUri, String pMatchString) {
 		assertTrue("Resource not found: " + pUri, pUris.contains(pUri));
-/*
 		if (pUris.contains(pUri)) {
 		} else {
 			pRepository.list((r) -> {
@@ -57,6 +56,5 @@ public class ClassLoaderResourceRepositoryTest {
 				}
 			});
 		}
-*/
 	}
 }
