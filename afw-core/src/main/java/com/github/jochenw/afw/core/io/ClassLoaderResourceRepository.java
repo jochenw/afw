6311330daf917b59a6eec3318b19f23e338832b3
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2020-11-26 22:14:52 +0100

    More logging for the ClassLoaderResourceRepositoryTest.

--- afw-core/src/main/java/com/github/jochenw/afw/core/io/ClassLoaderResourceRepository.java
+++ afw-core/src/main/java/com/github/jochenw/afw/core/io/ClassLoaderResourceRepository.java
@@ -48,7 +48,7 @@ public class ClassLoaderResourceRepository implements IResourceRepository {
 				final URL url = en.nextElement();
 				log("list: url=" + url);
 				if ("jar".equals(url.getProtocol())) {
-					log("list: Jar URL detected, looking for jar file");
+					log("list: Jar URL detected, looking for jar file: " + url.getFile());
 					final String fileUri = url.getFile();
 					final int offset = fileUri.indexOf('!');
 					if (offset == -1) {
