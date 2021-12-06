dd3e3cb37cd5d053b844ddf0f207d22516739433
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2021-12-06 14:11:30 +0100

    Restoring the old crypto code, as a working base line.

--- afw-core/src/main/java/com/github/jochenw/afw/core/crypt/DefaultKeyHandler.java
+++ afw-core/src/main/java/com/github/jochenw/afw/core/crypt/DefaultKeyHandler.java
@@ -11,57 +11,59 @@ import java.security.cert.Certificate;
 import com.github.jochenw.afw.core.util.Reflection;
 import com.github.jochenw.afw.core.util.Tupel;
 
-/** Default implementation of {@link IKeyHandler}. Attempts to use the {@link JceKeyHandler},
- * which should work on Java 8. If that doesn't work, uses the {@link BcKeyHandler}.
+/** Default implementation of {@link IKeyHandler}. Uses JCE, if
+ * possible, otherwise BouncyCastle.
  */
 public class DefaultKeyHandler implements IKeyHandler {
-	private static final IKeyHandler keyHandler;
-	static {
-		IKeyHandler kh;
+	private static final IKeyHandler handler = newDefaultKeyHandler();
+
+	private static IKeyHandler newDefaultKeyHandler() {
 		try {
-			final Object x509CertInfo = Reflection.newObject("sun.security.x509.X509CertInfo");
-			kh = new JceKeyHandler();
+			// Check, whether the JCE Crypto code is available.
+			Reflection.newObject("sun.security.x509.X509CertInfo");
+			// If so, use the JceKeyHandler.
+			return new JceKeyHandler();
 		} catch (Throwable t) {
-			kh = new BcKeyHandler();
+			// Otherwise, use the BcKeyHandler.
+			return new BcKeyHandler();
 		}
-		keyHandler = kh;
 	}
 
 	@Override
 	public KeyPair createKeyPair() {
-		return keyHandler.createKeyPair();
+		return handler.createKeyPair();
 	}
 
 	@Override
 	public Certificate generateCertificate(String pDn, KeyPair pKeyPair, int pValidity) {
-		return keyHandler.generateCertificate(pDn, pKeyPair, pValidity);
+		return handler.generateCertificate(pDn, pKeyPair, pValidity);
 	}
 
 	@Override
 	public KeyStore createKeyStore(PrivateKey pPrivateKey, Certificate pCertificate, String pAlias, String pStoreType,
 			String pStorePass, String pKeyPass) {
-		return keyHandler.createKeyStore(pPrivateKey, pCertificate, pAlias, pStoreType, pStorePass, pKeyPass);
+		return handler.createKeyStore(pPrivateKey, pCertificate, pAlias, pStoreType, pStorePass, pKeyPass);
 	}
 
 	@Override
 	public void createKeyStore(OutputStream pOut, PrivateKey pPrivateKey, Certificate pCertificate, String pAlias,
 			String pStoreType, String pStorePass, String pKeyPass) {
-		keyHandler.createKeyStore(pOut, pPrivateKey, pCertificate, pAlias, pStoreType, pStorePass, pKeyPass);
+		handler.createKeyStore(pOut, pPrivateKey, pCertificate, pAlias, pStoreType, pStorePass, pKeyPass);
 	}
 
 	@Override
 	public Tupel<PrivateKey, Certificate> readPrivateKey(InputStream pIn, String pAlias, String pStorePass,
 			String pKeyPass) {
-		return keyHandler.readPrivateKey(pIn, pAlias, pStorePass, pKeyPass);
+		return handler.readPrivateKey(pIn, pAlias, pStorePass, pKeyPass);
 	}
 
 	@Override
 	public byte[] encrypt(Key pKey, byte[] pBytes) {
-		return keyHandler.encrypt(pKey, pBytes);
+		return handler.encrypt(pKey, pBytes);
 	}
 
 	@Override
 	public byte[] decrypt(Key pKey, byte[] pBytes) {
-		return keyHandler.encrypt(pKey, pBytes);
+		return handler.decrypt(pKey, pBytes);
 	}
 }
