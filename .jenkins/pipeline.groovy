100a63aa0acd402cedb3244468d8a403d89e8218
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2022-04-22 10:45:05 +0200

    Let Jenkins use the new parent project.

--- .jenkins/pipeline.groovy
+++ .jenkins/pipeline.groovy
@@ -5,7 +5,7 @@ pipeline {
         jdk 'Java8' 
     }
     stages {
-        stage ('afw-core') {
+        stage ('build') {
             steps {
                 withMaven(
                      // Maven installation declared in the Jenkins "Global Tool Configuration"
@@ -24,26 +24,7 @@ pipeline {
                 }
             }
         }
-        stage ('afw-bootstrap') {
-            steps {
-                withMaven(
-                     // Maven installation declared in the Jenkins "Global Tool Configuration"
-                     maven: 'Maven3',
-
-                    // Use `$WORKSPACE/.repository` for local repository folder to avoid shared repositories
-                    mavenLocalRepo: '.repository',
-                ) {
-				    script {
-				        if (isUnix()) {
-    			            sh 'mvn -fafw-bootstrap/pom.xml -Pjacoco -Dmaven.test.failure.ignore=true clean install'
-					    } else {
-    			            bat 'mvn.cmd -fafw-bootstrap/pom.xml -Pjacoco -Dmaven.test.failure.ignore=true clean install'
-					    }
-				    }
-                }
-            }
-        }
-		stage ('profiler') {
+		stage ('collect profiler data') {
 		    steps {
                 step( [ $class: 'JacocoPublisher' ] )
 			}
