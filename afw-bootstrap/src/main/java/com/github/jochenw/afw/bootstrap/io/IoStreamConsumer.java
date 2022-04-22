9e7f823c49c30574bcd9c0f1b2e7d556324d704b
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2022-04-22 10:46:33 +0200

    Discarding afw-bootstrap.

--- afw-bootstrap/src/main/java/com/github/jochenw/afw/bootstrap/io/IoStreamConsumer.java
+++
@@ -1,8 +0,0 @@
-package com.github.jochenw.afw.bootstrap.io;
-
-import java.io.IOException;
-import java.io.InputStream;
-
-public interface IoStreamConsumer {
-	public void accept(InputStream pIn) throws IOException;
-}
