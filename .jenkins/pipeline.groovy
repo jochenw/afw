755bd24883a1c393e2843187290ae8a35b130fe5
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2022-04-23 18:32:40 +0200

    Fixing the Jenkins built to run in the parent project.

--- .jenkins/pipeline.groovy
+++ .jenkins/pipeline.groovy
@@ -11,14 +11,14 @@ pipeline {
                      // Maven installation declared in the Jenkins "Global Tool Configuration"
                      maven: 'Maven3',
 
-                    // Use `$WORKSPACE/.repository` for local repository folder to avoid shared repositories
-                    mavenLocalRepo: '.repository',
+                     // Use `$WORKSPACE/.repository` for local repository folder to avoid shared repositories
+                     mavenLocalRepo: '.repository',
                 ) {
 				    script {
 				        if (isUnix()) {
-    			            sh 'mvn -fafw-core/pom.xml -Pjacoco -Dmaven.test.failure.ignore=true clean install'
+    			            sh 'mvn -fpom.xml -Pjacoco -Dmaven.test.failure.ignore=true clean install'
 					    } else {
-    			            bat 'mvn.cmd -fafw-core/pom.xml -Pjacoco -Dmaven.test.failure.ignore=true clean install'
+    			            bat 'mvn.cmd -fpom.xml -Pjacoco -Dmaven.test.failure.ignore=true clean install'
 					    }
 					}
                 }
