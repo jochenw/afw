4bb0665f57c6b78d9eccfb547a97903e85a00cc5
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2022-11-14 18:13:17 +0100

    Adding the ParentLastClassLoader.

---
+++ afw-core/src/main/java/com/github/jochenw/afw/core/util/Enumerations.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2022 Jochen Wiedmann
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jochenw.afw.core.util;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+
+/** Utility class for working with {@link Enumeration enumerations}.
+ */
+public class Enumerations {
+	/**
+	 * Converts the enumeration into a list with the same elements.
+	 * @param pEnum The enumeration, that is being converted.
+	 *   <em>Note:</em> The enumeration
+	 *   is being completely consumed by this method, and
+	 *   {@link Enumeration#hasMoreElements()} will return
+	 *   false, after executing this method.
+	 *   If that is a problem, use {@link #asIterator(Enumeration)},
+	 *   or {@link #asIterable(Enumeration)}.
+	 * @return A list with the same elements.
+	 */
+	public static <O> List<O> asList(Enumeration<O> pEnum) {
+		final List<O> list = new ArrayList<>();
+		while (pEnum.hasMoreElements()) {
+			list.add(pEnum.nextElement());
+		}
+		return list;
+	}
+
+	/**
+	 * Converts the enumeration into a list with the same elements.
+	 * @param pEnum The enumeration, that is being converted.
+	 *   <em>Note:</em> The enumeration is <em>not</em> consumed
+	 *   by this method. However, using the iterator will affect
+	 *   the state of the enumeration.
+	 * @return An iterator over the same elements.
+	 */
+	public static <O> Iterator<O> asIterator(Enumeration<O> pEnum) {
+		return new Iterator<O>(){
+			@Override
+			public boolean hasNext() {
+				return pEnum.hasMoreElements();
+			}
+
+			@Override
+			public O next() {
+				return pEnum.nextElement();
+			}
+			
+		};
+	}
+
+	/**
+	 * Converts the enumeration into an iterable with the same elements.
+	 * @param pEnum The enumeration, that is being converted.
+	 *   <em>Note:</em> The enumeration is <em>not</em> consumed
+	 *   by this method. However, using the iterable, or the
+	 *   iterator, that it creates, will affect the state of
+	 *   the enumeration.
+	 * @return An iterable over the same elements.
+	 */
+	public static <O> Iterable<O> asIterable(Enumeration<O> pEnum) {
+		return new Iterable<O>(){
+			@Override
+			public Iterator<O> iterator() {
+				return asIterator(pEnum);
+			}
+		};
+	}
+}
---
+++ afw-core/src/main/java/com/github/jochenw/afw/core/util/ParentLastClassLoader.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2022 Jochen Wiedmann
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */package com.github.jochenw.afw.core.util;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collection;
+
+
+/** A ClassLoader, which works like an URLClassLoader, except that it
+ * implements a parent-last lookup.
+ */
+public class ParentLastClassLoader extends ClassLoader {
+	private final URL[] urls;
+	private final URLClassLoader delegateWithParent;
+	private final URLClassLoader delegateWithoutParent;
+	
+	/** Creates a new instance, which loads classes from the given Jar files.
+	 * @param pParent The parent ClassLoader, to which loading is being
+	 *   delegated. 
+	 * @param pUrls URL's of jar files, from which classes are being loaded.
+	 */
+	public ParentLastClassLoader(ClassLoader pParent, URL... pUrls) {
+		final ClassLoader parent = Objects.notNull(pParent, Thread.currentThread().getContextClassLoader());
+		urls = Objects.requireNonNull(pUrls, "Urls");
+		delegateWithParent = new URLClassLoader(urls, parent);
+		delegateWithoutParent = new URLClassLoader(urls, null);
+	}
+
+	/** Creates a new instance, which loads classes from the given Jar files.
+	 * @param pUrls URL's of jar files, from which classes are being loaded.
+	 * @return The created instance.
+	 */
+	public static ParentLastClassLoader of(URL... pUrls) {
+		return new ParentLastClassLoader(Thread.currentThread().getContextClassLoader(), pUrls);
+	}
+
+	/** Creates a new instance, which loads classes from the given Jar files.
+	 * @param pUrls URL's of jar files, from which classes are being loaded.
+	 * @return The created instance.
+	 */
+	public static ParentLastClassLoader of(Collection<URL> pUrls) {
+		return of(Thread.currentThread().getContextClassLoader(), pUrls);
+	}
+
+	/** Creates a new instance, which loads classes from the given Jar files.
+	 * @param pParent The parent ClassLoader, to which loading is being
+	 *   delegated. 
+	 * @param pUrls URL's of jar files, from which classes are being loaded.
+	 * @return The created instance.
+	 */
+	public static ParentLastClassLoader of(ClassLoader pParent, Collection<URL> pUrls) {
+		return new ParentLastClassLoader(pParent, pUrls.toArray(new URL[pUrls.size()]));
+	}
+	
+	@Override
+	protected Class<?> findClass(String pName) throws ClassNotFoundException {
+		try {
+			return delegateWithoutParent.loadClass(pName);
+		} catch (ClassNotFoundException e) {
+			return delegateWithParent.loadClass(pName);
+		}
+	}
+}
