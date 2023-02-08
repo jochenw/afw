7bb1220e89961dc2ee57bed40d1a9edb58362ff0
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2023-02-08 17:40:29 +0100

    More work on the Rest clas

--- afw-core/src/main/java/com/github/jochenw/afw/core/util/Rest.java
+++ afw-core/src/main/java/com/github/jochenw/afw/core/util/Rest.java
@@ -1,5 +1,22 @@
+/*
+ * Copyright 2018 Jochen Wiedmann
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.jochenw.afw.core.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,7 +27,6 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.Base64;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,15 +36,37 @@ import javax.annotation.Nullable;
 import javax.inject.Inject;
 
 import com.github.jochenw.afw.core.function.Functions.FailableConsumer;
+import com.github.jochenw.afw.core.function.Functions.FailableFunction;
 import com.github.jochenw.afw.core.function.Functions.FailableSupplier;
 import com.github.jochenw.afw.core.io.IReadable;
 import com.github.jochenw.afw.core.util.HttpConnector.HttpConnection;
 
 import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 
+
+/** Utility class for working with REST requests. Suggested use:
+ * <pre>
+ *   final Rest rest = getComponentFactory().requireInstance(Rest.class);
+ *   rest.builder(url)
+ *       .method("POST")
+ *       .resource("/rest/MyResource")
+ *       .resourceId(myResourceId)
+ *       .basicAuth("MyUsername", "Mypassword")
+ *       .parameter("MyParam", "Value")
+ *       .send()
+ * </pre>
+ * 
+ * If your request is supposed to create a result, replace {@link Builder#send()}
+ * with {@link Builder#call(FailableFunction)}.
+ *       
+ * </pre>
+ */
 public class Rest {
 	private @Inject HttpConnector httpConnector;
 
+	/** This class is a container for the REST requests configuration.
+	 * To create an instance, use a {@link Rest#builder(URL)}.
+	 */
 	public class Request {
 		private final @NotNull URL url;
 		private final String method, resource, resourceId;
@@ -36,6 +74,18 @@ public class Rest {
 		private final FailableConsumer<OutputStream,?> body;
 		private final FailableConsumer<InputStream,?> consumer, errorConsumer;
 
+		/** Creates a new instance. You are not supposed to use this constructor
+		 * directly. Instead, use {@link Rest#builder(URL)}.
+		 * @param pUrl The REST API's URL.
+		 * @param pMethod The HTTP method, which is being used.
+		 * @param pResource The REST resource.
+		 * @param pResourceId The REST resource id.
+		 * @param pParameters The map of request parameters.
+		 * @param pHeaders The map of request headers.
+		 * @param pBody An optional producer for the request body.
+		 * @param pConsumer An optional consumer for the response body.
+		 * @param pErrorConsumer An optional consumer for the error response body.
+		 */
 		public Request(URL pUrl, String pMethod, String pResource, String pResourceId, Map<String, String> pParameters,
 				Map<String, String> pHeaders, FailableConsumer<OutputStream, ?> pBody,
 				FailableConsumer<InputStream, ?> pConsumer, FailableConsumer<InputStream, ?> pErrorConsumer) {
@@ -50,17 +100,83 @@ public class Rest {
 			errorConsumer = pErrorConsumer;
 		}
 
+		/** Returns the REST API's URL. The actual URL is built by concatenating this URL,
+		 * the {@link #getResource() resource URI}, and the {@link #getResourceId() resource id}.
+		 * @return The REST API's URL
+		 * @see #getResource()
+		 * @see #getResourceId()
+		 */
 		public URL getUrl() { return url; }
+		/** Returns the HTTP request method, like POST, PUT, DELETE, or GET.
+		 * May be null, in which case GET is used as the default.
+		 * @return The HTTP request method, or null.
+		 * @see Rest.Builder#post(FailableConsumer)
+		 * @see Rest.Builder#put(FailableConsumer)
+		 */
 		public String getMethod() { return method; }
+		/** Returns the REST resource URI, which will be appended to the {@link #getUrl() REST API URL}.
+		 * @return The REST resource URI
+		 * @see #getUrl()
+		 * @see #getResourceId()
+		 */
 		public String getResource() { return resource; }
+		/** Returns the optional REST resource id, which will be appended to the {@link #getUrl() REST API URL},
+		 * and the {@link #getResource() REST resource URI}.
+		 * @return The REST resource id
+		 * @see #getUrl()
+		 * @see #getResource()
+		 */
 		public String getResourceId() { return resourceId; }
+		/** Returns the map of HTTP request parameters.
+		 * <em>Note:</em> As of this writing, multiple values per parameter are unsupported.
+		 * @return The map of HTTP request parameters.
+		 * @see Rest.Builder#parameter(String, String)
+		 * @see #getHeaders()
+		 */
 		public Map<String, String> getParameters() { return parameters; }
+		/** Returns the map of HTTP request parameters.
+		 * <em>Note:</em> As of this writing, multiple values per header are unsupported.
+		 * @return The map of HTTP request parameters.
+		 * @see Rest.Builder#header(String, String)
+		 * @see #getParameters()
+		 */
 		public Map<String, String> getHeaders() { return headers; }
+		/** Returns a producer for the request body.
+		 * <em>Note:</em> Keep in mind, that the {@link #getMethod() HTTP request method}
+		 * must be "POST", or "PUT", if you have a request body. The best way to ensure this
+		 * is to use the methods {@link Rest.Builder#post(FailableConsumer)}, or
+		 * {@link Rest.Builder#put(FailableConsumer)}, rather than
+		 * {@link Rest.Builder#body(FailableConsumer)}.
+		 * @return The producer for the request body, or null.
+		 */
 		public FailableConsumer<OutputStream, ?> getBody() { return body; }
+		/** Returns a consumer for the response body.
+		 * <em>Note:</em> If your consumer is supposed to produce a result,
+		 * consider using a {@link Rest#call(Request, FailableFunction) result
+		 * function} instead.
+		 * @return The consumer for the response body, or null.
+		 */
 		public FailableConsumer<InputStream, ?> getConsumer() { return consumer; }
+		/** Returns a consumer for the error response body.
+		 * @return The consumer for the error response body, or null.
+		 */
 		public FailableConsumer<InputStream, ?> getErrorConsumer() { return errorConsumer; }
+		/** Performs the request by invoking {@link Rest#send(Request)}. Invokes the
+		 * {@link #getBody() body producer}, {@link #getConsumer() response consumer},
+		 * and the {@link #getErrorConsumer() error response consumer}, as necessary.
+		 * If your response is supposed to produce a result, consider using
+		 * {@link #call(FailableFunction)} instead.
+		 */
 		public void send() {
 			Rest.this.send(this);
+		}
+		/** Performs the request by invoking {@link Rest#send(Request)}. Invokes the
+		 * {@link #getBody() body producer}, the {@code pCallable result producer},
+		 * the {@link #getConsumer() response consumer},
+		 * and the {@link #getErrorConsumer() error response consumer}, as necessary.
+		 */
+		public <O> O call(FailableFunction<InputStream,O,?> pCallable) {
+			return Rest.this.call(this, pCallable);
 		}
 	}
    
@@ -221,9 +337,43 @@ public class Rest {
 		public void send() {
 			Rest.this.send(build());
 		}
+
+		public <O> O call(FailableFunction<InputStream, O, ?> pCallable) {
+			return Rest.this.call(build(), pCallable);
+		}
+	}
+
+	public <O> O call(Request pRequest, FailableFunction<InputStream,O,?> pCallable) {
+		final Request request = Objects.requireNonNull(pRequest, "Request");
+		final FailableFunction<InputStream,O,?> callable = Objects.requireNonNull(pCallable, "Callable");
+		final FailableConsumer<InputStream,?> consumer = request.getConsumer();
+		final Holder<O> holder = new Holder<>();
+		if (consumer == null) {
+			final FailableConsumer<InputStream,?> reader = (in) -> {
+				holder.set(callable.apply(in));
+			};
+			send(request, reader);
+			return holder.get();
+		} else {
+			final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			final FailableConsumer<InputStream,?> reader = (in) -> Streams.copy(in, baos);
+			send(request, reader);
+			final byte[] bytes = baos.toByteArray();
+			try {
+				consumer.accept(new ByteArrayInputStream(bytes));
+				holder.set(callable.apply(new ByteArrayInputStream(bytes)));
+				return holder.get();
+			} catch (Throwable t) {
+				throw Exceptions.show(t);
+			}
+		}
 	}
 
 	public void send(Request pRequest) {
+		send(pRequest, pRequest.getConsumer());
+	}
+
+	protected void send(Request pRequest, FailableConsumer<InputStream,?> pReader) {
 		try {
 			URL url = pRequest.getUrl();
 			final String res = pRequest.getResource();
@@ -282,7 +432,7 @@ public class Rest {
 				if (pRequest.getHeaders() != null) {
 					pRequest.getHeaders().forEach((n,v) -> urlConn.addRequestProperty(n, v));
 				}
-				if (pRequest.getConsumer() != null) {
+				if (pReader != null) {
 					urlConn.setDoInput(true);
 				}
 				if (pRequest.getBody() != null) {
@@ -301,9 +451,9 @@ public class Rest {
 					}
 					throw new IllegalStateException("Unexepected HTTP Response: " + status + "," + msg);
 				}
-				if (pRequest.getConsumer() != null) {
+				if (pReader != null) {
 					try (InputStream in = urlConn.getInputStream()) {
-						pRequest.getConsumer().accept(in);
+						pReader.accept(in);
 					}
 				}
 			}
