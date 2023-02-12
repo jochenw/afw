5716845ee7bc477645c3118e4f93f70a384e9c71
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2023-02-12 10:59:46 +0100

    Adding changes.xml

--- afw-core/src/main/java/com/github/jochenw/afw/core/util/Streams.java
+++ afw-core/src/main/java/com/github/jochenw/afw/core/util/Streams.java
@@ -796,7 +796,7 @@ public class Streams {
      * @param pIn The input stream, which supplies the contents.
      * @param pObservers The observers, which are to be participating in the
      *   incoming data.
-     * return The created input stream. By reading this, you will obtain data, that
+     * @return The created input stream. By reading this, you will obtain data, that
      *   is actually read from {@code pIn}, but ths observers will be notified
      *   about the same data.
      */
