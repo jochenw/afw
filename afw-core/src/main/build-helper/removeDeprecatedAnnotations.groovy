18a8ebb3c77d1e33e0a0061905a09f2e0c2cae2c
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2023-11-14 10:20:30 +0100

    Adding Groovy script, that fixes generated files with an invalid @deprecated annotation.

---
+++ afw-core/src/main/build-helper/removeDeprecatedAnnotations.groovy
@@ -0,0 +1,23 @@
+/*
+ * Remove @deprecated annotations from
+ * target/generated-sources/javacc/com/github/jochenw/afw/core/el/jcc/SimpleCharStream.java
+ */
+final java.nio.file.Path path =
+    java.nio.file.Paths.get("target/generated-sources/javacc/com/github/jochenw/afw/core/el/jcc/SimpleCharStream.java");
+if (!java.nio.file.Files.isRegularFile(path)) {
+    throw new IllegalStateException("File not found: " + path);
+}
+List<String> lines = java.nio.file.Files.readAllLines(path, java.nio.charset.StandardCharsets.UTF_8);
+int numberOfLinesRemoved = 0;
+for (int i = 0;  i < lines.size();) {
+    String line = lines.get(i);
+    if ("   * @deprecated".equals(line)) {
+        lines.remove(i);
+        numberOfLinesRemoved++;
+	} else {
+	    // Ignore this line, go to the next one.
+	    i++;
+	}
+}
+System.out.println("Number of @deprecated annotations removed: " + numberOfLinesRemoved);
+java.nio.file.Files.write(path, lines, java.nio.charset.StandardCharsets.UTF_8);
